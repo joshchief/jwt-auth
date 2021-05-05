d79a490f00a8e9cf8d3adc5fc1b42aed4c6bd31e
Nnamani Chinaecherem <nnamanijoshua@Nnamanis-MacBook-Pro.local>
2021-05-05 16:53:41 +0100

    another commit

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +18,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// create user route
+Route::get('/user-create', function(Request $request){
+    $user = User::create([
+        'name' => 'Joshua Nnamani',
+        'email' => 'joshchief169@gmail.com',
+        'password' => Hash::make("chinaecherem12")
+    ]);
+
+    return response()->json([
+        'new user' => $user
+    ], 201);
+});
+
+
+// login route
+Route::post('/login', function(Request $request){
+
+    $credentials = $request->only('email','password');
+
+    if ( ! $token = Auth::attempt($credentials)){
+        return response()->json(['error' => 'unauthorized'], 401);
+    }
+    return $token;
+});
+
+
+// get authenticated user info
+Route::middleware('auth')->get('/me', function(){
+    return response()->json(Auth::user(), 200);
+});
+
+// logout use
+Route::get('/getout', function(){
+    auth()->logout();
+    return response()->json(['message' => 'successfully logged out']);
 });
